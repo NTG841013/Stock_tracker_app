28706cafadf4d51238fa0b4989ae73f2e2443123
Nhlanhla Khumalo <ntg567497@gmail.com>
2025-10-20 22:55:09 +0200

    search functionality implementation

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,37 +1,40 @@
 'use client'
 
-
 import {NAV_ITEMS} from "@/lib/constants";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
-import {cn} from "@/lib/utils";
+import SearchCommand from "@/components/SearchCommand";
 
-interface NavItemsProps {
-    className?: string;
-}
-
-const NavItems = ({ className }: NavItemsProps) => {
+const NavItems = ({initialStocks}: { initialStocks: StockWithWatchlistStatus[]}) => {
     const pathname = usePathname()
 
     const isActive = (path: string) => {
         if (path === '/') return pathname === '/';
+
         return pathname.startsWith(path);
     }
 
     return (
-        <ul className={cn("flex flex-col sm:flex-row gap-3 sm:gap-8 font-medium", className)}>
-            {NAV_ITEMS.map(({href, label}) => (
-                <li key={href}>
-                    <Link
-                        href={href}
-                        className={`transition-colors hover:text-yellow-500 ${
-                            isActive(href) ? 'text-gray-100' : 'text-gray-400'
-                        }`}
-                    >
+        <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+            {NAV_ITEMS.map(({ href, label }) => {
+                if(href === '/search') return (
+                    <li key="search-trigger">
+                        <SearchCommand
+                            renderAs="text"
+                            label="Search"
+                            initialStocks={initialStocks}
+                        />
+                    </li>
+                )
+
+                return <li key={href}>
+                    <Link href={href} className={`hover:text-yellow-500 transition-colors ${
+                        isActive(href) ? 'text-gray-100' : ''
+                    }`}>
                         {label}
                     </Link>
                 </li>
-            ))}
+            })}
         </ul>
     )
 }
